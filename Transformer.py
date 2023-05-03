b01fedb3e882edf9519b732453f37e6c6c64e143
Arda <ardaras3@gmail.com>
2023-05-03 23:11:46 +0300

    Update Transformer.py
    + Timestamp is changed to string type on final schema before sending to big query

--- Transformer.py
+++ Transformer.py
@@ -1,6 +1,6 @@
 # Deployment options => pyspark --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.3
 
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType, FloatType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
 from pyspark.sql.functions import col, from_json, to_json, struct
 
 # InvoiceNo,StockCode,Description,Quantity,InvoiceDate,UnitPrice,CustomerID,Country
@@ -9,12 +9,13 @@ baseSchema = StructType([
     StructField("StockCode", StringType(), nullable=False),
     StructField("Description", StringType(), nullable=True),
     StructField("Quantity", IntegerType(), nullable=False),
-    StructField("InvoiceDate", TimestampType(), nullable=True),
+    StructField("InvoiceDate", StringType(), nullable=True),
     StructField("UnitPrice", FloatType(), nullable=False),
     StructField("CustomerID", IntegerType(), nullable=True),
     StructField("Country", StringType(), nullable=True)
 ])
 
+KAFKA_PARAMS = "34.125.166.117:9092"
 SUBSCRIBE_TOPIC = "RawData"
 PUBLISH_TOPIC = "TransformedData"
 CHECKPOINT_DIR = "/home/arda_aras_dev/checkpointdir2"
@@ -22,7 +23,7 @@ CONSUMER_GROUP_ID = "Transformers"
 
 # Subscribe to RawData topic
 df = spark.readStream.format("kafka") \
-        .option("kafka.bootstrap.servers", "34.125.112.207:9092") \
+        .option("kafka.bootstrap.servers", KAFKA_PARAMS) \
         .option("failOnDataLoss", False) \
         .option("subscribe", SUBSCRIBE_TOPIC) \
         .load()
@@ -34,7 +35,7 @@ finalDf = parsed_df.selectExpr("data.InvoiceNo AS InvoiceNo",
                  "data.StockCode AS StockCode",
                  "data.Description AS Description",
                  "data.Quantity AS Quantity",
-                 "CAST(data.InvoiceDate AS TIMESTAMP) AS InvoiceDate",
+                 "data.InvoiceDate AS InvoiceDate",
                  "data.UnitPrice AS UnitPrice",
                  "data.CustomerID AS CustomerID",
                  "data.Country AS Country")
@@ -45,9 +46,8 @@ cleanedFinalDf = finalDf.dropDuplicates().na.drop()
 kafkaDf = cleanedFinalDf.selectExpr("to_json(struct(*)) AS value") \
     .writeStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "34.125.112.207:9092") \
+    .option("kafka.bootstrap.servers", KAFKA_PARAMS) \
     .option("checkpointLocation", CHECKPOINT_DIR) \
     .option("failOnDataLoss", False) \
-    .option("kafka.group.id", CONSUMER_GROUP_ID) \
     .option("topic", PUBLISH_TOPIC) \
     .start()
